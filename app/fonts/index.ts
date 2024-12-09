ed05072493feccfe79697dabb374ca9797fa9db5
Franciszek Czajka <fczajka@Edith.local>
2024-12-09 15:20:40 +0100

    Add font and further configuration

---
+++ app/fonts/index.ts
@@ -0,0 +1,13 @@
+import { Josefin_Sans, Literata } from 'next/font/google';
+
+const defaultFont = Josefin_Sans({
+  display: 'swap',
+  weight: '400',
+  variable: '--font-default',
+});
+
+const headlineFont = Literata({
+  display: 'swap',
+  weight: '400',
+  variable: '--font-headline',
+});
