5e3d318e8a41749a251ca2ddec0f5b74edea3adb
Franciszek Czajka <franekczajkaa@gmail.com>
2024-12-10 23:17:38 +0100

    Add custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,13 @@ export default {
   ],
   theme: {
     extend: {
+      colors: {
+        'off-white': '#FAF9F6',
+        seafoam: '##9BE5BE',
+        sunset: '##9BE5BE',
+        lavender: '#BA9CD6',
+        night: '#1C1C1C',
+      },
       fontFamily: {
         headline: ['Josefin Sans', 'ui-sans-serif', 'system-ui'],
         default: ['Literata', 'ui-serif', 'Georgia'],
