a96fe98326955e881b7bc0d265100baf3b4309c8
Franciszek Czajka <fczajka@192.168.1.135>
2024-12-12 23:20:19 +0100

    Display some data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import { Navbar } from '@/ui';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -13,7 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className='font-default antialiased'>{children}</body>
+      <body className='font-default antialiased'>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,30 @@
-export default function Home() {
+import type { Recipe } from '@/globals/interface';
+
+export default async function Home() {
+  const data = await fetch(
+    `${process.env.MAIN_URL}/recipes/findByIngredients?ingredients=apples,+flour,+sugar&ranking=1&apiKey=${process.env.API_KEY}`,
+  );
+  const recipes: Recipe[] = await data.json();
+
+  //https://api.spoonacular.com/food/ingredients/search?query=banana&sortDirection=desc&apiKey={process.env.API_KEY}
+
   return (
-    <div>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec
-    </div>
+    <main>
+      <div>
+        {recipes.map((recipe) => (
+          <div key={recipe.id} className='p-4 mb-4 border rounded shadow-lg'>
+            <h2 className='text-xl font-bold mb-2'>{recipe.title}</h2>
+            <h3 className='text-lg font-semibold'>Used Ingredients:</h3>
+            <ul className='list-disc list-inside mb-2'>
+              {recipe.usedIngredients.map((ingredient) => (
+                <li key={ingredient.id} className='ml-4'>
+                  {ingredient.name}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </main>
   );
 }
